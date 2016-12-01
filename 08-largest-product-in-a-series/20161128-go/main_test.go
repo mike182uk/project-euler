2d7d7488bac6dbd7a57a4024eed05383938dcbbb
Michael Barrett <mike182uk@gmail.com>
2016-12-01 16:38:33 +0000

    add go solution for problem 08

---
+++ 08-largest-product-in-a-series/20161128-go/main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFindLargestProductInSeries(t *testing.T) {
+	assert.Equal(t, 5832, findLargestProductInSeries(num, 4))
+}
